50e858c90299c23f898958fc95b906c746c973ee
yrigny <yrigny@student.42.fr>
2024-11-29 20:02:15 +0100

    save draft

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: yrigny <yrigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 21:37:25 by yrigny            #+#    #+#             */
-/*   Updated: 2024/11/28 23:07:26 by yrigny           ###   ########.fr       */
+/*   Updated: 2024/11/29 20:01:49 by yrigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ BitcoinExchange::BitcoinExchange()
 		throw NoDatabaseException();
 	while (getline(database, line))
 	{
-		// substr (0, find(,)) -> date
-
-		// substr (find(,) + 1) -> rate
-
-		// add to map
-
+		size_t	pos = line.find(',');
+		if (pos == std::string::npos)
+			throw NoDatabaseException();
+		std::string	date = line.substr(0, pos);
+		std::string	price = line.substr(pos + 1);
+		std::cout << date << "," << price << std::endl;
+		double	priceInDouble = strtod(price.c_str(), NULL);
+		_exchangeRate[date] = priceInDouble;
 	}
 	database.close();
 }
@@ -53,11 +55,40 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& rhs)
 
 void	BitcoinExchange::calculateValue(std::string inputLine)
 {
-	// parse input date and amount
+	std::string	date;
+	double		amount;
+	double		price;
 
+	/* check bad input */
+	if (inputLine.length() < 14 || inputLine.find(" | ") == std::string::npos)
+	{
+		std::cout << "Error: bad input => " << inputLine << std::endl;
+		return;
+	}
+	/* parse input date and amount */
+	if (inputLine.length() >= 14)
+	{
+		date = inputLine.substr(0, 10);
+		if (!isValidDate(date))
+		{
+			std::cout << "Error: bad input => " << date << std::endl;
+			return;
+		}
+		amount = strtod(inputLine.substr(13).c_str(), NULL);
+		if (amount <= 0)
+		{
+			std::cout << "Error: bad input => " << amount << std::endl;
+			return;
+		}
+	}
 	// find exchange rate for date
-
+	price = _exchangeRate.at(date);
 	// calculate value
+}
+
+bool	BitcoinExchange::isValidDate(std::string)
+{
+	
 }
 
 const char*	BitcoinExchange::NoDatabaseException::what() const throw()
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: yrigny <yrigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 21:37:52 by yrigny            #+#    #+#             */
-/*   Updated: 2024/11/28 22:50:08 by yrigny           ###   ########.fr       */
+/*   Updated: 2024/11/29 18:14:11 by yrigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define BITCOINEXCHANGE_HPP
 # include <fstream>
 # include <map>
-# include <string>
+# include <iostream>
+# include <cstdlib>
+
 class BitcoinExchange
 {
 	private:
@@ -27,6 +29,7 @@ class BitcoinExchange
 		
 		BitcoinExchange&	operator=(const BitcoinExchange& rhs);
 		void				calculateValue(std::string inputLine);
+		bool				isValidDate(std::string date);
 
 		class NoDatabaseException : std::exception
 		{
